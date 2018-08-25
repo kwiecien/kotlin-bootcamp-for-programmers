2f3d5e6ad5c2773dcac22f9469d0e313d38a4bbf
kwiecien <k.d.kwiecinski@gmail.com>
2018-08-25 21:01:59 +0200

    feat: Main arguments

---
+++ src/AMS.kt
@@ -0,0 +1,36 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    println("Hello, ${args[0]}!")
+    dayOfTheWeek()
+    expression()
+    ifExpression(10)
+}
+
+fun dayOfTheWeek() {
+    println("What day is today?")
+    val day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+    println(when (day) {
+        1 -> "Sunday"
+        2 -> "Monday"
+        3 -> "Tuesday"
+        4 -> "Wednesday"
+        5 -> "Thursday"
+        6 -> "Friday"
+        7 -> "Saturday"
+        else -> "???"
+    })
+}
+
+fun expression() {
+    val unit = println("This is an expression")
+    println(unit)
+}
+
+fun ifExpression(temperature: Int) {
+    val isHot = if (temperature > 50) true else false
+    println(isHot)
+
+    val message = "You are ${if (temperature > 50) "fried" else "safe"} fish"
+    println(message)
+}
