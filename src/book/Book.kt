5927d3209fadf9dfa899af7e8883049af341b683
kwiecien <k.d.kwiecinski@gmail.com>
2018-08-27 21:00:31 +0200

    feat: Pairs and Triples

--- src/book/Book.kt
+++ src/book/Book.kt
@@ -1,15 +1,31 @@
 package book
 
-open class Book(val title: String, val author: String) {
+fun main(args: Array<String>) {
+    val book = Book("TDD", "Beck", 1990)
+    val (title, author, year) = book.getAllData()
+    val bookTitleAuthor = book.getAllData()
+    println("The best book: $title by $author (${bookTitleAuthor.third})")
+}
+
+open class Book(val title: String, val author: String, val year: Int) {
 
     private var currentPage = 1
 
     open fun readPage() {
         currentPage++
     }
+
+    fun getTitleAndAuthor(): Pair<String, String> {
+        return title to author
+    }
+
+    fun getAllData(): Triple<String, String, Int> {
+        return Triple(title, author, year)
+    }
+
 }
 
-class eBook(title: String, author: String, var format: String = "text") : Book(title, author) {
+class eBook(title: String, author: String, var format: String = "text") : Book(title, author, 1) {
 
     private var wordsRead = 0
 
