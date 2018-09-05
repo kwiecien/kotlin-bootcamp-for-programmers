49fbb2bd8f843e5d664882fb0dab6a33e5cb0c2b
kwiecien <k.d.kwiecinski@gmail.com>
2018-09-05 23:49:24 +0200

    game: Play basic game

--- src/game/Game.kt
+++ src/game/Game.kt
@@ -1,5 +1,36 @@
 package game
 
 class Game {
-    var path: MutableList<Direction> = mutableListOf(Direction.START)
+    var path = mutableListOf(Direction.START)
+
+    val north = { path.add(Direction.NORTH) }
+    val south = { path.add(Direction.SOUTH) }
+    val east = { path.add(Direction.EAST) }
+    val west = { path.add(Direction.WEST) }
+
+    val end = {
+        path.add(Direction.END)
+        println("Game Over: $path")
+        path.clear()
+        false
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val game = Game()
+    printPath(game)
+    play(game)
+    printPath(game)
+}
+
+fun printPath(game: Game) {
+    println(game.path)
+}
+
+fun play(game: Game) {
+    game.north()
+    game.south()
+    game.west()
+    game.end()
 }
