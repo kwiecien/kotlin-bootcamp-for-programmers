808cf95591b4bb05fe9e0e65a95155fef0f6c729
kwiecien <k.d.kwiecinski@gmail.com>
2018-09-06 23:49:10 +0200

    game: Full NSWE game

--- src/game/Game.kt
+++ src/game/Game.kt
@@ -1,36 +1,41 @@
 package game
 
 class Game {
-    var path = mutableListOf(Direction.START)
+    private var path = mutableListOf(Direction.START)
 
-    val north = { path.add(Direction.NORTH) }
-    val south = { path.add(Direction.SOUTH) }
-    val east = { path.add(Direction.EAST) }
-    val west = { path.add(Direction.WEST) }
+    private val north = { path.add(Direction.NORTH) }
+    private val south = { path.add(Direction.SOUTH) }
+    private val east = { path.add(Direction.EAST) }
+    private val west = { path.add(Direction.WEST) }
 
-    val end = {
+    private val end = {
         path.add(Direction.END)
         println("Game Over: $path")
         path.clear()
         false
     }
 
+    fun makeMove(command: String?) {
+        when (command) {
+            "n" -> move(north)
+            "s" -> move(south)
+            "w" -> move(west)
+            "e" -> move(east)
+            else -> move(end)
+        }
+
+    }
+
+    private fun move(where: () -> Boolean) {
+        where.invoke()
+    }
+
 }
 
 fun main(args: Array<String>) {
     val game = Game()
-    printPath(game)
-    play(game)
-    printPath(game)
-}
-
-fun printPath(game: Game) {
-    println(game.path)
-}
-
-fun play(game: Game) {
-    game.north()
-    game.south()
-    game.west()
-    game.end()
+    println("Enter a direction: n/s/w/e: ")
+    while (true) {
+        game.makeMove(readLine())
+    }
 }
